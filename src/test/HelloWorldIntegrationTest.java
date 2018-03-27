54c174b0c4e25f593bfc6d6c7f7d71b70497d4da
Christoffer Nissen <christoffer.nissen@gmail.com>
2018-03-27 17:49:25 +0200

    Changed test to startup server on its own

--- src/test/HelloWorldIntegrationTest.java
+++ src/test/HelloWorldIntegrationTest.java
@@ -1,5 +1,9 @@
+import com.Rethink.dropwizarddemo.DropwizardApplication;
+import com.Rethink.dropwizarddemo.DropwizardConfiguration;
 import com.Rethink.dropwizarddemo.POJO.Donor;
+import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.Rule;
 import org.junit.Test;
 
 import javax.ws.rs.client.Client;
@@ -7,17 +11,18 @@ import javax.ws.rs.client.Client;
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldIntegrationTest {
-    /*
-        @Rule
-        public final DropwizardAppRule<DropwizardConfiguration> RULE =
-                new DropwizardAppRule<DropwizardConfiguration>(DropwizardApplication.class,
-                        ResourceHelpers.resourceFilePath("hello-world.yml"));
-    */
+
+    // Starts the program before tests are run
+    @Rule
+    public final DropwizardAppRule<DropwizardConfiguration> RULE =
+            new DropwizardAppRule<DropwizardConfiguration>(DropwizardApplication.class);
+
     @Test
     public void runServerTest() {
         Client client = new JerseyClientBuilder().build();
+
         Donor result = client.target(
-                String.format("http://localhost:8080/donors/1") // , RULE.getLocalPort())
+                String.format("http://localhost:%d/donors/1", RULE.getLocalPort())
         ).request().get(Donor.class);
 
         assertEquals(result.getId(), 1);
